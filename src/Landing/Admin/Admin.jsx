93cbafae75fcabd738e9f243cca23b1414f7be72
Harikrishnan <harikrishnanb24@gmail.com>
2023-09-20 02:34:59 +0530

    image add cheyyada

--- src/Landing/Admin/Admin.jsx
+++ src/Landing/Admin/Admin.jsx
@@ -1,23 +1,18 @@
-import React from 'react'
-import './Admin.css'
+import React from "react";
+import "./Admin.css";
 
 const Admin = () => {
   return (
-    <div className='page'>
-        <div className="userinput">
-        <input type="number" placeholder='Enter team number'/>
-        <button>
-            submit
-        </button>
-        </div>
-        
-        <br />
-        <button>
-            Release
-        </button>
-    </div>
-    
-  )
-}
+    <div className="page">
+      <div className="userinput">
+        <input type="number" placeholder="Enter team number" />
+        <button>submit</button>
+      </div>
 
-export default Admin
+      <br />
+      <button>Release</button>
+    </div>
+  );
+};
+
+export default Admin;
--- src/Landing/Client/Client.jsx
+++ src/Landing/Client/Client.jsx
@@ -5,16 +5,11 @@ const Client = () => {
   return (
     <div>
    <div className="page">
-    <div className="header">
-       
-        
-
-        
         <br />
         <h6>presents</h6>
         <br />
         <h1>GEN AI Workshop</h1>
-    </div>
+    
     <div className="userinput">
         <input type="text" placeholder='Enter your name'/>
         <button>
