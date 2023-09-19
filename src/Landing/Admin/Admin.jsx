ae5438088abbdb6d082a88bb7272ce77f965c00b
Harikrishnan <harikrishnanb24@gmail.com>
2023-09-20 01:48:57 +0530

    admin page updation

---
+++ src/Landing/Admin/Admin.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Admin.css'
+
+const Admin = () => {
+  return (
+    <div className='page'>
+        <div className="userinput">
+        <input type="number" placeholder='Enter team number'/>
+        <button>
+            submit
+        </button>
+        </div>
+        
+        <br />
+        <button>
+            Release
+        </button>
+    </div>
+    
+  )
+}
+
+export default Admin
--- src/Landing/Client/Client.jsx
+++ src/Landing/Client/Client.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Client.css'
 
 const Client = () => {
   return (
